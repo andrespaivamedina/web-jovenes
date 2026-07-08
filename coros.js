48198c209f1d888e1d8adb55610f37e4fbd65633
andrespaivamedina <andres.paiva.medina@gmail.com>
2026-07-08 15:02:29 -0400

    Create coros.js

---
+++ coros.js
@@ -0,0 +1,52 @@
+const baseDeDatosCoros = [
+    {
+        id: 1,
+        titulo: "Yendo Saulo",
+        letra: "Yendo Saulo camino a Damasco\nUn resplandor le cegó\n// Y cayendo en tierra\nUna voz se escuchó //\n\nSaulo, Saulo, ¿por qué me persigues?\nSo oyó una voz celestial\n// A quién tu persigues\nPues a Él, le servirás //"
+    },
+    {
+        id: 2,
+        titulo: "Yo tengo gozo",
+        letra: "Yo tengo gozo, gozo, en mi corazón,\n// en mi corazón //\nYo tengo gozo, gozo, en mi corazón,\n¡gloria al Salvador!\n\nYo tengo paz perfecta en mi corazón,\n// en mi corazón //\nYo tengo paz perfecta en mi corazón,\n¡gloria al Salvador!\n\nYo tengo alegría en mi corazón,\n// en mi corazón //\nYo tengo alegría en mi corazón,\n¡gloria al Salvador!\n\nYo tengo gozo, paz y alegría en mi corazón,\n// en mi corazón //\nYo tengo gozo, paz y alegría en mi corazón,\n¡gloria al Salvador!"
+    },
+    {
+        id: 3,
+        titulo: "Yo vivo Señor",
+        letra: "Yo vivo Señor porque Tú vives\nPorque Tú vives Señor es que yo vivo\nMe das consuelo, me das abrigo\nY en la aflicción, mi Señor está conmigo\n\nSoy salvo, Señor, pues me salvaste\npues me salvaste, Señor eternamente.\nYo voy al cielo, voy a la gloria\nporque oh Señor, Tú me diste la victoria"
+    },
+    {
+        id: 4,
+        titulo: "Zaqueo",
+        letra: "Zaqueo, pequeñito era,\ny a causa de la multitud,\na un sicómoro subió,\n//pues a Cristo quiso ver//\n\nY cuando el Salvador pasó,\ny el árbol contempló,\nÉl exclamó: ¡Zaqueo, ven!\nA tu casa yo iré,\ny contigo cenaré.\n\nZaqueo fue a su casa,\ny allí encontró al Señor,\ny devolvió con creces,\n//lo que antes defraudó//\n\nSi tú como Zaqueo,\naceptas al Señor,\ntendrás ahora mismo,\n//puro y limpio corazón//"
+    },
+    {
+        id: 5,
+        titulo: "¿Para qué pecar?",
+        letra: "¿Para qué pecar si soy salvo,\nsi ya tengo tanta luz?\n¿Para qué pecar si la vida\nDios me ha dado en Jesús?\n¿Para qué pecar si el infierno\nsolo espera al pecador?\n// ¿Para qué pecar? ¿Para qué pecar?\n¿Para qué pecar contra Dios? //"
+    },
+    {
+        id: 6,
+        titulo: "¿Quién es Él?",
+        letra: "¿Quién es Él, que con baldón,\nnace fuera del mesón?\nEs Jesús, ¡oh qué historia!\nEs Jesús, el Rey de gloria,\nA sus pies es mi lugar\nAlabando sin cesar.\n\n¿Quién es Él, por hombres mil,\nlevantado en cruz tan vil?\nEs Jesús, ¡oh qué historia!\nEs Jesús, el Rey de gloria,\nA sus pies es mi lugar\nAlabando sin cesar.\n\n¿Quién es Él, que al resucitar,\npoder tiene para salvar?\nEs Jesús, ¡oh qué historia!\nEs Jesús, el Rey de gloria,\nA sus pies es mi lugar\nAlabando sin cesar."
+    },
+    {
+        id: 7,
+        titulo: "¿Quién hizo lindas flores?",
+        letra: "¿Quién hizo lindas flores?\nNi tú, ni yo, ¿pues quién?\n¿Quién hizo lindas flores?\nTan solo el Señor.\n\n¿Quién hizo los pececitos?\nNi tú, ni yo, ¿pues quién?\n¿Quién hizo los pececitos?\nTan solo el Señor.\n\n¿Quién hizo a los animales?\nNi tú, ni yo, ¿pues quién?\n¿Quién hizo a los animales?\nTan solo el Señor.\n\n¿Quién hizo a los niñitos?\nNi tú, ni yo, ¿pues quién?\n¿Quién hizo a los niñitos?\nTan solo el Señor.\n\n¿Quién puede a ti salvarte?\nNi tú, ni yo, ¿pues quién?\n¿Quién puede a ti salvarte?\nTan solo el Señor."
+    },
+    {
+        id: 8,
+        titulo: "0. AVISO PRÓXIMA REU",
+        letra: "PRÓXIMA REUNIÓN:\nSábado 12 de agosto\n17:00 HRS."
+    },
+    {
+        id: 9,
+        titulo: "0. THE FINAL COUNTDOWN",
+        letra: "LA REUNIÓN COMENZARÁ\nA LAS 18:50"
+    },
+    {
+        id: 10,
+        titulo: "A combatir",
+        letra: "¡A combatir! resuena la guerrera voz\nDel Salvador, que hoy llamando está.\nSin desmayar, seguidle siempre con valor,\nY la victoria plena os dará.\n\n¡A la batalla, oh fiel Cristiano!\nCon el escudo de la cruz;\nSé buen soldado, pues a tu lado,\nestá el príncipe Jesús.\nÉl con su gracia te sostiene,\nY con potencia sin igual;\nSu brazo extiende, y te defiende,\nEn esta lucha contra el mal.\n\nAl Rey de reyes, nuestro Salvador Jesús,\nHonor y gloria todos tributad,\nPues ya los suyos gozan de Su plenitud,\nY pronto reinarán en santidad."
+    }
+];
